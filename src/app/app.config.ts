536f3e72313127ba4ab00a8f4025600e41f0a738
nguyentr02 <truongphamtuannguyen@gmail.com>
2024-06-01 02:22:57 +1000

    Upload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { routes } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
+NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+  providers: [],
+  imports: [
+    MatToolbarModule,
+  ],
+})
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes)],
 };
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  showjob1 = true;
+  showjob2 = false;
+  async ngOnInit(): Promise<void> {
+    // const navbar = <HTMLElement>document.getElementsByClassName('navbar')[0];
+    // // console.log(this.navbar);
+    // let temp: any=0;
+    // onscroll = (event) => {
+    //   console.log(window.scrollY);
+    //   console.log('Temp is ',temp);
+    //   if (window.scrollY < temp) {
+    //     navbar.style.display = 'absolute';
+    //   }
+    //   temp = window.scrollY;
+    // };
+    const about = <HTMLElement>document.getElementsByClassName('about-nav')[0];
+    about.onclick = function () {
+      changePagePosAbout();
+    };
+
+    onscroll = (event) => {
+      console.log(window.scrollY);
+    };
+
+    const exp = <HTMLElement>document.getElementsByClassName('exp-nav')[0];
+    exp.onclick = function () {
+      changePagePosExp();
+    };
+
+    const cnt = <HTMLElement>document.getElementsByClassName('cnt-nav')[0];
+    cnt.onclick = function () {
+      changePagePosCnt();
+    }
+    function changePagePosAbout() {
+      window.scrollTo(0, 705.5999755859375);
+    }
+
+    function changePagePosExp() {
+      window.scrollTo(0, 1325);
+    }
+
+    function changePagePosCnt() {
+      window.scrollTo(0,1950);
+    }
+  }
+
+  async toggleShowjob1() {
+    this.showjob1 = true;
+    this.showjob2 = false;
+  }
+
+  async toggleShowjob2() {
+    this.showjob1 = false;
+    this.showjob2 = true;
+  }
+}
